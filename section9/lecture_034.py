b717d3e166fc06efd9ac5a1d1df2e13805ffe63a
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-03-16 13:09:06 +0530

    indent

--- section9/lecture_034.py
+++ section9/lecture_034.py
@@ -1,16 +1,10 @@
 # The code below should be run in IDLE or directly in the Python command line.
 
 password = 'tony'
-
 password == 'tony'
-
 output: True
-
 password =='Tony'
-
 output: False
-
 password1 = 'Frank'
 password1.lower() == 'frank'
-
 output: True
